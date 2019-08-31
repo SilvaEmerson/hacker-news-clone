743f7c701f63128aacb1b334e00f43ada6d7b3c5
SilvaEmerson <emersonjose877@gmail.com>
2019-08-30 22:22:46 -0300

    Fix grapql queries

--- back/hackernewsclone/graphql_schema.py
+++ back/hackernewsclone/graphql_schema.py
@@ -13,10 +13,12 @@ class WriterType(DjangoObjectType):
     class Meta:
         model = Writer
 
+
 class Query(graphene.ObjectType):
     all_posts = graphene.List(PostType)
     all_writers = graphene.List(WriterType)
-    post = graphene.Field(PostType, id=graphene.Int(), title=graphene.String())
+    get_posts = graphene.List(PostType, title=graphene.String(), writer=graphene.String())
+    get_post = graphene.Field(PostType, id=graphene.Int())
 
     def resolve_all_writers(self, info, **kwargs):
         return Writer.objects.all()
@@ -24,19 +26,19 @@ class Query(graphene.ObjectType):
     def resolve_all_posts(self, info, **kwargs):
         return Post.objects.select_related('writer').all()
 
-    def resolve_post(self, info, **kwargs):
-        id = kwargs.get('id', None)
+    def resolve_get_posts(self, info, **kwargs):
         title = kwargs.get('title', None)
         writer = kwargs.get('writer', None)
         
         if writer:
-            return Post.objects.select_related('writer').get(name=writer)
+            return Post.objects.select_related('writer').filter(writer__name=writer)
 
         if title:
-            return Post.objects.select_related('title').get(name=writer)
+            return Post.objects.filter(title__contains=title)
 
-        if id:
-            return Post.objects.select_related('id').get(name=writer)
+    def resolve_get_post(self, info, **kwargs):
+        return Post.objects.get(pk=kwargs.get('id', None)) 
+
 
 schema = graphene.Schema(
     query=Query
