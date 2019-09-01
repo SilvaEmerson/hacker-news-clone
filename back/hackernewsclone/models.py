5d6e3a5cf52eea0753fd7a8ad1c302d31c32ca14
SilvaEmerson <emersonjose877@gmail.com>
2019-09-01 09:08:48 -0300

    Update model

--- back/hackernewsclone/models.py
+++ back/hackernewsclone/models.py
@@ -1,8 +1,13 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Writer(models.Model):
     name = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.name
 
     def __repr__(self):
         return f"Writer[name={self.name}]"
@@ -10,7 +15,13 @@ class Writer(models.Model):
 
 class Post(models.Model):
     title = models.CharField(max_length=250)
-    author = models.OneToOneField(Writer, on_delete=models.DO_NOTHING)
+    author = models.ForeignKey(Writer, on_delete=models.DO_NOTHING)
 
     def __repr__(self):
         return f"Post[title={self.title}, author={self.author.name}]"
+
+
+@receiver(post_save, sender=Post)
+def handle_new_post(*args, **kwargs):
+    print(args)
+    print(kwargs)
