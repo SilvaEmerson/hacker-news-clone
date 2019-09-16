0e0e3e7f65b5da1e8594248b9efd68c73a30c840
SilvaEmerson <emersonjose877@gmail.com>
2019-09-16 19:40:24 -0300

    Update forms validation

--- back/hackernewsclone/forms.py
+++ back/hackernewsclone/forms.py
@@ -1,23 +1,25 @@
 from django import forms
+from django.contrib.auth.models import User
 
-from hackernewsclone.models import Post, Writer
+from hackernewsclone.models import Post
 
 
 class PostForm(forms.ModelForm):
     author = forms.ModelChoiceField(
-        queryset=Writer.objects.all().values_list("name", flat=True).distinct()
+        queryset=User.objects.all().values_list("username", flat=True).distinct()
     )
 
     class Meta:
         model = Post
-        fields = ["title", "author"]
+        fields = ["title", "author", "content"]
 
     def is_valid(self):
         title = self.data.get("title", None)
         author = self.data.get("author", None)
+        content = self.data.get("content", None)
 
-        if author and title:
-            if Writer.objects.filter(name=author).exists():
+        if author and title and content:
+            if User.objects.filter(username=author).exists() and len(content) > 0:
                 return True
         return False
 
@@ -25,14 +27,15 @@ class PostForm(forms.ModelForm):
         if self.is_valid():
             obj = Post(
                 title=self.data["title"],
-                author=Writer.objects.get(name=self.data["author"]),
+                author=User.objects.get(username=self.data["author"]),
+                content=self.data["content"],
             )
             obj.save()
             return obj
         return None
 
 
-class WriterForm(forms.ModelForm):
+class UserForm(forms.ModelForm):
     class Meta:
-        model = Writer
-        fields = ["name"]
+        model = User
+        fields = ["username", "password", "email"]
