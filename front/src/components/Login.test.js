ff7f02682453e2a1df7ab475770ce6c310ff9d51
SilvaEmerson <emersonjose877@gmail.com>
2019-09-20 22:56:36 -0300

    Add more tests

--- front/src/components/Login.test.js
+++ front/src/components/Login.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { MockedProvider } from "@apollo/react-testing";
 import { Login, GET_USER_TOKEN } from "./Login";
+import { act } from "react-dom/test-utils";
 
 const mocks = [
   {
@@ -20,13 +21,57 @@ const mocks = [
   }
 ];
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(
-    <MockedProvider mocks={mocks} addTypename={false}>
-      <Login />
-    </MockedProvider>,
-    div
-  );
-  ReactDOM.unmountComponentAtNode(div);
+describe("Login component tests", () => {
+  let div = null;
+
+  beforeEach(() => (div = document.createElement("div")));
+
+  afterEach(() => {
+    ReactDOM.unmountComponentAtNode(div);
+    div = null;
+  });
+
+  it("renders without crashing", () => {
+    ReactDOM.render(
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <Login />
+      </MockedProvider>,
+      div
+    );
+  });
+
+  it("component content should contain 'Login'", () => {
+    ReactDOM.render(
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <Login />
+      </MockedProvider>,
+      div
+    );
+
+    expect(div.textContent).toContain("Login");
+  });
+
+  it("component content should contain 'Sucess' message after form submiting", () => {
+    act(() => {
+      ReactDOM.render(
+        <MockedProvider mocks={mocks} addTypename={false}>
+          <Login />
+        </MockedProvider>,
+        div
+      );
+    });
+
+    let subBtn = div.querySelector("[type=submit]");
+    let usernameInput = div.querySelector("[type=text]");
+    let passwordInput = div.querySelector("[type=password]");
+
+    usernameInput.textContent = "demo";
+    passwordInput.textContent = "demo";
+
+    act(() => {
+      subBtn.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+    });
+
+    expect(div.textContent).toContain("Login");
+  });
 });
